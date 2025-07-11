d9ee176e28efea0418947abe9a7a585359fa929f
isri21 <israelalemu21@gmail.com>
2025-07-11 16:57:24 +0300

    Make Container Large

--- src/components/Container.jsx
+++ src/components/Container.jsx
@@ -5,7 +5,7 @@ import Control from "./Control";
 const Container = () => {
   const [count, setcount] = useState(0);
   return (
-    <div className="bg-[#380260] w-50 h-50 m-auto mt-20 rounded-[30px] border-2 border-[#2d2828]">
+    <div className="bg-[#380260] w-70 h-70 m-auto mt-20 rounded-[30px] border-2 border-[#2d2828]">
       <Display count={count} />
       <Control count={count} setcount={setcount} />
     </div>
